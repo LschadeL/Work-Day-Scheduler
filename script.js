a74362c1cad5b0e1276d1c59e124d1771078d34b
Lillith Schade <loganniles96@gmail.com>
2024-04-09 15:42:43 -0500

    created functions for saving and adjusting local storage data

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+currentTime = dayjs().hour();
+
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
+    $("#currentDay").text(dayjs().format("dddd, MMMM D"));
+    showTimes();
+    $("button.saveBtn").click(createSchedule);
+    $("#clearBtn").click(clearSchedule);
+});
+
+function showTimes() {
+   const timeId = $(this).attr("id");
+   const savedData = localStorage.getItem(timeId);
+   const hourBlock = parseInt(timeId.split("-")[1]);
+   const currentMinute = currentTime.minute(0);
+   const timeBlock = dayjs().hour(hourBlock).minute(0);
+
+   $(".time-block").each(function () {
+        if (savedData !== null) {
+            $(this).children(".description").val(savedData);
+        };
+
+        if (timeBlock.isBefore(currentMinute, "hour")) {
+            $(this).removeClass("present future").addClass("past");
+        } else if (timeBlock.isSame(currentMinute, "hour")) {
+            $(this).removeClass("past future").addClass("present");
+        } else {
+            $(this).removeClass("past present").addClass("future");
+        }
+    });
+};
+
+function createSchedule() {
+    const timeId = $(this).parent().attr("id");
+    const userInput = $(this).siblings(".description").val();
+
+    localStorage.setItem(timeId, userInput);
+};
+  
+function clearSchedule() {
+    localStorage.clear();
+    location.reload();
+};
